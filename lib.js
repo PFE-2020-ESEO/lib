a83168c4926b8b3e4e52cd0b0813956e3aeda8d2
juju <bardin.julien41@gmail.com>
2020-11-13 11:05:31 +0100

    no polling telegram

--- lib.js
+++ lib.js
@@ -28,7 +28,7 @@ const telegram = {
 }
 // replace the value below with the Telegram token you receive from @BotFather
 const tokenTelegram = '1444178058:AAG8flYFGoNPACwIMwLrf45J7urud8b0I3k';
-const botTelegram = new TelegramBot(tokenTelegram, {polling: true});
+const botTelegram = new TelegramBot(tokenTelegram);
 
 
 ///////////////////////
