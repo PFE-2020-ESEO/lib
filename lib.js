2c75220e699c4460ed3f8b7db10fa7f4b943e174
juju <bardin.julien41@gmail.com>
2020-11-27 08:38:23 +0100

    send mail csv

--- lib.js
+++ lib.js
@@ -14,14 +14,14 @@ var nodemailer = require('nodemailer');
 ////////////////////////////////////
 
 const mail = {
-    "email" : "contact@actics.ovh",
-    "password" : "contact@actics.ovh",
-    "host":"ssl0.ovh.net",
-    "port":587
+    "email": "contact@actics.ovh",
+    "password": "contact@actics.ovh",
+    "host": "ssl0.ovh.net",
+    "port": 587
 }
 
 const telegram = {
-    "adminId" : [
+    "adminId": [
         "501762868" // julien
         // "243096262" // paul
     ]
@@ -38,15 +38,15 @@ const botTelegram = new TelegramBot(tokenTelegram);
 ///////////////////////
 
 
-exports.sendMail = (user,obj,text) => {
+exports.sendMail = (user, obj, text) => {
 
     let transporter = nodemailer.createTransport({
         host: mail.host,
         port: mail.port,
         secure: false, // true for 465, false for other ports
         auth: {
-          user: mail.email, // generated ethereal user
-          pass: mail.password, // generated ethereal password
+            user: mail.email, // generated ethereal user
+            pass: mail.password, // generated ethereal password
         },
     });
 
@@ -55,10 +55,61 @@ exports.sendMail = (user,obj,text) => {
         to: user, // list of receivers
         subject: obj, // Subject line
         text: text // plain text body
-      });
+    });
 }
 
-exports.sendTelegram = (userId,text) => {
+exports.sendMailCSV = (user, obj, text, jsondata) => {
+
+    const transporter = nodemailer.createTransport({
+        host: mail.host,
+        port: mail.port,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: mail.email, // generated ethereal user
+            pass: mail.password, // generated ethereal password
+        },
+    });
+
+    //some data
+    const data = jsondata
+
+    const keys = Object.keys(data[0]);
+    //conver the data to CSV with the column names
+    const csv = parse(data, keys);
+
+    transporter.sendMail(
+        {
+            from: '"Actics 👻" <contact@actics.ovh>', // sender address
+            to: user, // list of receivers
+            subject: obj, // Subject line
+            text: text, // plain text body,
+            // here is the magic
+            attachments: [
+                {
+                    filename: 'file.csv',
+                    content: csv
+                }
+            ]
+        },
+        (err, info) => {
+            if (err) {
+                console.log('Error occurred. ' + err.message);
+                return process.exit(1);
+            }
+
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        }
+    );
+
+}
+
+
+
+
+
+exports.sendTelegram = (userId, text) => {
     botTelegram.sendMessage(userId, text);
 }
 
