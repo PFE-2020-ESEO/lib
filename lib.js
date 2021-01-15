56d77cdbf473e26de5fc2a0c753005916ffdd863
juju <bardin.julien41@gmail.com>
2021-01-15 16:17:20 +0100

    sendMailwithHtml

--- lib.js
+++ lib.js
@@ -59,6 +59,28 @@ exports.sendMail = (user, obj, text) => {
     });
 }
 
+
+exports.sendMailWithHtml = (user, obj, text,html) => {
+
+    let transporter = nodemailer.createTransport({
+        host: mail.host,
+        port: mail.port,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: mail.email, // generated ethereal user
+            pass: mail.password, // generated ethereal password
+        },
+    });
+
+    let info = transporter.sendMail({
+        from: '"Actics 👻" <contact@actics.ovh>', // sender address
+        to: user, // list of receivers
+        subject: obj, // Subject line
+        text: text, // plain text body
+        html:html
+    });
+}
+
 exports.sendMailCSV = (user, obj, text, jsondata) => {
 
     const transporter = nodemailer.createTransport({
@@ -84,6 +106,55 @@ exports.sendMailCSV = (user, obj, text, jsondata) => {
             to: user, // list of receivers
             subject: obj, // Subject line
             text: text, // plain text body,
+            // here is the magic
+            attachments: [
+                {
+                    filename: 'file.csv',
+                    content: csv
+                }
+            ]
+        },
+        (err, info) => {
+            if (err) {
+                console.log('Error occurred. ' + err.message);
+                return process.exit(1);
+            }
+
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        }
+    );
+
+}
+
+
+exports.sendMailCSVWithHtml = (user, obj, text, html, jsondata) => {
+
+    const transporter = nodemailer.createTransport({
+        host: mail.host,
+        port: mail.port,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: mail.email, // generated ethereal user
+            pass: mail.password, // generated ethereal password
+        },
+    });
+
+    //some data
+    const data = jsondata
+
+    const keys = Object.keys(data[0]);
+    //conver the data to CSV with the column names
+    const csv = parse(data, keys);
+
+    transporter.sendMail(
+        {
+            from: '"Actics 👻" <contact@actics.ovh>', // sender address
+            to: user, // list of receivers
+            subject: obj, // Subject line
+            text: text, // plain text body,
+            html:html,
             // here is the magic
             attachments: [
                 {
